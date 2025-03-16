e2c43fcf448729f0625b7ac79771095d9f3efce1
Jisun696 <istiakkashem35@gmail.com>
2025-03-16 23:22:34 +0600

    Day 01 task done

---
+++ Day-01/task/task.js
@@ -0,0 +1,5 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.getElementById("root").innerText = "Check the browser console for a message!!!!";
+
+// document.write("Check the browser console for a message!!!!"); // ! task.js:4 Failed to execute 'write' on 'Document': It isn't possible to write into a document from an asynchronously-loaded external script unless it is explicitly opened. (defer attribute)
