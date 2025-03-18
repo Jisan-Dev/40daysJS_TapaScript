2b97ff3d8ee9124fff4050e9732ad64bd0ea4b67
Jisun696 <istiakkashem35@gmail.com>
2025-03-18 19:23:50 +0600

    Day-03 task 3 & 4 done

--- Day-03/index.js
+++ Day-03/index.js
@@ -13,3 +13,22 @@ if (age >= 18) {
 } else {
   console.log("You are not eligible");
 }
+
+// 3. Calculate CTC with a Bonus
+const monthlySalary = 12300;
+// calculate yearly salary with 20% bonus on annual salary
+const yearlySalary = monthlySalary * 12;
+const bonus = yearlySalary * 0.2;
+const totalSalary = yearlySalary + bonus;
+console.log(totalSalary);
+
+// 4. Write a program for the Traffic Light Simulation.
+const color = "green";
+switch (color) {
+  case "red":
+    console.log("STOP!");
+    break;
+  case "green":
+    console.log("GO!");
+    break;
+}
