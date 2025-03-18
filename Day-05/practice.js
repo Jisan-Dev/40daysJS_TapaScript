1cdb9bda6d5d400f0b100b5a0f3db9a39f0ce0df
Jisun696 <istiakkashem35@gmail.com>
2025-03-19 00:19:59 +0600

    Day-05 practice

---
+++ Day-05/practice.js
@@ -0,0 +1,47 @@
+// for loop
+// “A for loop is best when we know exactly how many times we need to run a block of code.”
+
+/*
+ for (initialization; condition; update) {
+    // Code
+ }
+*/
+for (let count = 1; count <= 5; count++) {
+  console.log("Iteration/Loop", count);
+}
+
+// Addition of even numbers between 1 to 100
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+    // console.log(sum);
+  }
+}
+console.log("Sum is", sum);
+
+let language = "JavaScript";
+for (let i = 0; i < language.length; i++) {
+  // console.log(language[i]);
+  console.log(language.charAt(i));
+}
+
+// Nested Loop
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log("Row", i, "Col", j);
+  }
+}
+
+// Break and Continue
+for (let i = 1; i <= 3; i++) {
+  if (i === 3) break;
+  console.log(i);
+}
+
+console.log("-----------------");
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue;
+  console.log(i);
+}
