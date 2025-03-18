a79239d6c20e7f89f5b5d768dffc20a62d743675
Jisun696 <istiakkashem35@gmail.com>
2025-03-18 22:36:36 +0600

    Day-04 task 2  done

--- Day-04/task.js
+++ Day-04/task.js
@@ -9,3 +9,11 @@ switch (day) {
     console.log("It's a normal day.");
 }
 // Output: It's a normal day. It is because the variable 'day' containing the value 'Monday' that starts with capital M. in the switch case statement we are checking with the vale 'monday' that starts with smaller 'm' hence that condition will not apply and the default operations will be applied.
+
+// 2. Build an ATM Cash Withdrawal System
+const amount = 200;
+if (amount % 100 === 0) {
+  console.log("Withdrawal successful");
+} else {
+  console.log("Invalid amount");
+}
