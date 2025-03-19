aa7dabd038bd2c0a040544d80dde463cbc76076b
Jisun696 <istiakkashem35@gmail.com>
2025-03-19 17:54:26 +0600

    Day-05 task 4 done

--- Day-05/task.js
+++ Day-05/task.js
@@ -29,3 +29,12 @@ for (let i = 1; i <= 500; i++) {
   }
 }
 console.log(sum);
+
+//4. Skipping Multiples of 3
+// Write a program to print numbers from 1 to 20, but skip multiples of 3.
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  console.log(i);
+}
