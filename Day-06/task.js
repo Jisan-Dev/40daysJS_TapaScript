0ec685293724479046979f6ad516db7d730eb8b5
Jisun696 <istiakkashem35@gmail.com>
2025-03-20 19:43:22 +0600

    Day-06 task2 done

--- Day-06/task.js
+++ Day-06/task.js
@@ -3,3 +3,13 @@ function celsiusToFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32 + "°F";
 }
 console.log(celsiusToFahrenheit(30));
+
+// 2. Create a Function to Find the Maximum of Two Numbers
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(findMax(10, 5));
