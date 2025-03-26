19bcf9ec1a0f6b1be0fa2d7ab1d7c9fdd00591c4
Jisun696 <istiakkashem35@gmail.com>
2025-03-26 17:09:06 +0600

    day-11 task-3 done

--- Day-11/task.js
+++ Day-11/task.js
@@ -22,3 +22,14 @@ function testClosure() {
 console.log(testClosure()()); // 100
 
 // reason : reason is closure inner function access outer function and multiply x * x. first the testClosure() func executes, the x variable closed to the returned inner function, then the inner function executes using the closed "X" variable value.
+
+// 3. Create a button dynamically and attach a click event handler using a closure. The handler should count and log how many times the button was clicked.
+function buttonClick() {
+  let number = 0;
+  document.getElementById("handleClick").addEventListener("click", function () {
+    number++;
+    console.log("Button Click", number, "Item");
+  });
+}
+
+buttonClick();
