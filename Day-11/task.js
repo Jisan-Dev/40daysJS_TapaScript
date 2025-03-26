68451e2c0851dfbb54ff04deb4a9c4a95ef4adbc
Jisun696 <istiakkashem35@gmail.com>
2025-03-26 16:58:27 +0600

    day-11 task-2 done

--- Day-11/task.js
+++ Day-11/task.js
@@ -11,3 +11,14 @@ counter(); // 1
 counter(); // 2
 
 //reason: we know, when a function declared inside an outer function and that inner function has the access of all the variables from the outer function even after the execution of the outer function, that is called closure. when it closed on some variable it always remembers the previous value of those variables.
+
+// 2. What will be the output and why?
+function testClosure() {
+  let x = 10;
+  return function () {
+    return x * x;
+  };
+}
+console.log(testClosure()()); // 100
+
+// reason : reason is closure inner function access outer function and multiply x * x. first the testClosure() func executes, the x variable closed to the returned inner function, then the inner function executes using the closed "X" variable value.
