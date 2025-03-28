9a87fe6fc1d6a43676050ab3861ccda4a68c7934
Jisun696 <istiakkashem35@gmail.com>
2025-03-29 00:05:06 +0600

    day-12 task-2 done

--- Day-12/task.js
+++ Day-12/task.js
@@ -6,3 +6,14 @@ console.log(user.age ?? "Not provided");
     the output will be 'Not provided', because of Nullish coalescing operator. (?)
     it returns the right hand operand if the left hand one is undefined or null
 */
+
+// 2. What will happen if we try to modify a frozen object?
+
+const object = Object.freeze({ a: 1 });
+object.a = 2;
+console.log(object.a); // 1
+
+/* SOLUTION (for 2)
+    nothing, we'll only get the original properties cause when we use freeze method means,
+    we can't do any thing with object that become freeze.
+*/
