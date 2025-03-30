99e14445a17a61d5f5293000fb147899b8ac7b80
Jisun696 <istiakkashem35@gmail.com>
2025-03-30 17:48:19 +0600

    first task done

---
+++ Day-13/task.js
@@ -0,0 +1,12 @@
+//1. Create a table of two columns, `situation` and `value`. Now add the rows for every situations and the value of `this` in that situation. Please cover the following situations
+/**
+  - At the Global
+  - Inside an Object Method
+  - Inside the Standalone non-Arrow Function
+  - Inside an Arrow Function(standalone)
+  - Inside an Arrow Function(as object method)
+  - Inside an object created with the Constructor Function
+
+  Please add examples for each of the scenarios.
+ */
+/*SOLUTION : go to "./thisKeyword.png" */
