b6894a02815a016e1a42ff160946710fc7a3f1c5
Jisun696 <istiakkashem35@gmail.com>
2025-03-30 17:57:16 +0600

    day-13 task-2 done

--- Day-13/task.js
+++ Day-13/task.js
@@ -9,4 +9,20 @@
 
   Please add examples for each of the scenarios.
  */
-/*SOLUTION : go to "./thisKeyword.png" */
+/*SOLUTION(for q1): go to "./thisKeyword.png" */
+
+//__________________________________________________________________________________
+
+//2.  What is the problem here? Fix it to log the correct name and explain the fix
+/*
+const user = {
+  name: "tapaScript",
+  greet: () => {
+    console.log(`Hello, ${this.name}!`);
+  },
+};
+
+user.greet();
+*/
+
+/*SOLUTION (for q2):: The reason we're getting undefined from ${this.name} is because as per the rules, if the 'this' keyword used inside a arrow func it always check in the parent scope of where the arrow function is lexically placed(lexical scope). to fix this we can use this console.log(`Hello, ${this.name}!`); inside another block like {console.log(`Hello, ${this.name}!`)} or we can convert the arrow function to normal traditional function "function(){console.log(`Hello, ${this.name}!`);}" */
