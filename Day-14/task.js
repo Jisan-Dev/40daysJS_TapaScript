047a9ff36c0efb19c78971342fb2004edd4978e2
Jisun696 <istiakkashem35@gmail.com>
2025-04-17 00:07:37 +0600

    task3 of day14

--- Day-14/task.js
+++ Day-14/task.js
@@ -25,7 +25,7 @@ function processPayment(amount) {
       console.log("Valid amount");
     }
   } catch (error) {
-    console.log("An error occurred:", error);
+    console.error("An error occurred:", error);
   }
 }
 
@@ -40,3 +40,96 @@ processPayment(-1000);
  * - ServerError
  * - EmailError
  */
+
+// UserError implementation
+function UserError(message) {
+  this.name = "UserError";
+  this.message = message;
+  this.stack = new Error().stack;
+}
+
+UserError.prototype = Object.create(Error.prototype);
+
+function validateUserAge(age) {
+  if (age < 18) {
+    throw new UserError("Forbidden User!");
+  }
+  return "User signed up";
+}
+
+try {
+  const msg = validateUserAge(19);
+  console.log("user validation:", msg);
+} catch (error) {
+  console.error(`${error.name}: ${error.message}`);
+}
+
+// PaymentError implementation
+function PaymentError(message) {
+  this.name = "PaymentError";
+  this.message = message;
+  this.stack = new Error().stack;
+}
+PaymentError.prototype = Object.create(Error.prototype);
+
+function validatePayment(amount) {
+  if (!isNaN(amount)) {
+    throw new PaymentError("Invalid payment input");
+  }
+  return "Payment Successful!";
+}
+
+try {
+  const msg = validatePayment("1200");
+  console.log(msg);
+} catch (error) {
+  console.error(`${error.name}: ${error.message}`);
+  console.log(error.stack);
+}
+
+// ServerError -
+function ServerError(message) {
+  this.name = "ServerError";
+  this.message = message;
+  this.stack = new Error().stack;
+}
+
+ServerError.prototype = Object.create(Error.prototype);
+
+function checkServerError(serverStatus) {
+  if (!serverStatus) {
+    throw new ServerError(`${serverStatus}: server request was not complete .......`);
+  }
+  console.log(`Server request was completed`);
+}
+
+try {
+  checkServerError(false);
+} catch (error) {
+  console.error(`${error.name}: ${error.message} ; stack: ${error.stack}`);
+}
+
+// EmailError -
+
+function EmailError(message) {
+  this.name = "EmailError";
+  this.message = message;
+  this.stack = new Error().stack;
+}
+
+EmailError.prototype = Object.create(Error.prototype);
+
+function checkEmail(email) {
+  if (!email.includes("@")) {
+    throw new EmailError(`${email}: is an Invalid email`);
+  }
+  console.log(`Valid email good to go`);
+}
+
+try {
+  checkEmail("lrem123gmail.com");
+} catch (error) {
+  console.error(`${error.name}: ${error.message}`);
+}
+
+//___________________________________________________________________________
